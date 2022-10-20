3db10c8563368dac2621edecc8b39e8704045964
IaDrake20 <ios.drake05@gmail.com>
2022-10-20 07:58:38 -0400

    schedule command can take 2 different sets of params

--- #pybot.py
+++ #pybot.py
@@ -1,5 +1,7 @@
-#pybot
+#pybot made by Ian Drake
+
 #from imaplib
+from cgi import print_form
 import discord
 import requests
 import youtube_dl
@@ -38,11 +40,48 @@ async def on_mesage(message):
         for item in soup.find_all('img'):
             print(item['src'])
             
-    #if message.content.startswith('$Schedule')
+@client.event
+#message should contain $Schedule, (channel_id), (daily/weekly), (hour), (message)
+async def scheduler(message):
+    if message.content.startswith('$Schedule'):
+        seperated_message = message.rsplit(",",4)
+        #set channel_id frm message
+        channel_id = seperated_message[1]
+        
+        #set message to daily or weekly
+        time_dw = seperated_message[2]
+        
+        #set message to send at specified hour
+        time_h = seperated_message[3] 
+        
+        #messge to be sent
+        msg = seperated_message[4]
+        
+@client.event
+#message should contain $Schedule, (daily/weekly), (hour), (message)
+async def scheduler(message):
+    
+    target_channel_id = 0000
+    
+    if message.content.startswith('$Schedule'):
+        seperated_message = message.rsplit(",",3)
+        
+        #set message to daily or weekly
+        time_dw = seperated_message[1]
+        
+        #set message to send at specified hour
+        time_h = seperated_message[2] 
+        
+        #messge to be sent
+        msg = seperated_message[3] 
+        
+        @tasks.loop(hours= time_h)
+        if "day" in time_dw:
+            async def called_once_a_day():
+            message_channel = bot.get_channel(target_channel_id)
+            print(f"Got channel {message_channel}")
+            await message_channel.send(msg)
             
-@tasks.loop(hours=)
-async def called_once_a_day():
-    message_channel = bot.get_channel(target_channel_id)
-    #printf(f"Got channel {message_channel}")
-    await message_channel.send("")
+            
+
 client.run('MTAzMjM5ODc3MzE4MTIyNzAwOA.GS_z-I.ga5jGNrDJDcDM-_cwDgpth-5wYqxlP7ic39_9o')
